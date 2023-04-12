b0759d90d3c71bc7a1beac80e9115cd26e785746
guillem <4lon3ly0@tutanota.com>
2023-04-12 22:51:44 +0200

    Clean up codebase

--- src/option.rs
+++ src/option.rs
@@ -1,7 +1,5 @@
-#![allow(clippy::module_name_repetitions)]
 use std::{
     fmt::Debug,
-    marker::PhantomData,
     mem::MaybeUninit,
     sync::{
         atomic::{AtomicBool, Ordering},
@@ -9,15 +7,11 @@ use std::{
     },
 };
 
-// this codebase is a bit dirty from the pre-rwlock implementation.
-// ignore everything about an non-existent guard.
-
 /// Option with an atomic tag and interior synchronization.
 pub struct AtomicOption<T> {
-    // guard: AtomicBool,
     tag: AtomicBool,
     value: RwLock<MaybeUninit<T>>,
-    phantom: PhantomData<T>,
+    // phantom: PhantomData<T>,
 }
 
 /// Sets the tag to true after drop. Grabs a reference
@@ -33,31 +27,23 @@ impl<T> AtomicOption<T> {
     #[inline]
     pub const fn some(x: T) -> Self {
         Self {
-            // guard: AtomicBool::new(false),
             tag: AtomicBool::new(true),
             value: RwLock::new(MaybeUninit::new(x)),
-            phantom: PhantomData,
+            // phantom: PhantomData,
         }
     }
 
     #[inline]
     pub const fn none() -> Self {
         Self {
-            // guard: AtomicBool::new(false),
             tag: AtomicBool::new(false),
             value: RwLock::new(MaybeUninit::uninit()),
-            phantom: PhantomData,
+            // phantom: PhantomData,
         }
     }
 
-    /// The guard ensures the tag is set after initialization without
-    /// having this function move the new value preemptively.
     #[inline]
     pub fn try_set(&self) -> Option<SetGuard<'_, T>> {
-        // guard == true => return None
-        // guard == false && tag == true => return None
-        // guard == false && tag == false => set guard to true, return a guard
-        // if !self.guard.fetch_or(true, Ordering::AcqRel) && !self.tag.load(Ordering::Acquire) {
         if let Ok(guard) = self.value.try_write() && !self.tag.load(Ordering::Acquire) {
             Some(SetGuard{guard, tag: &self.tag, written: false})
         } else {
@@ -67,50 +53,22 @@ impl<T> AtomicOption<T> {
 
     #[inline]
     pub fn get(&self) -> Option<&RwLock<T>> {
-        // self.wait_acq_guard();
         if self.tag.load(Ordering::Acquire) {
-            // self.guard.store(false, Ordering::Release);
             Some(unsafe { &*(&self.value as *const RwLock<MaybeUninit<T>> as *const _) })
         } else {
-            // self.guard.store(false, Ordering::Release);
             None
         }
     }
 
-    // #[inline]
-    // pub fn try_empty(&self) -> Result<(), ()> {
-    //     // guard == true => nothing
-    //     // guard == false && tag == false => nothing
-    //     // guard == false && tag == true => set tag to none, drop inner
-    //     if !self.guard.load(Ordering::Acquire) && self.tag.fetch_and(false, Ordering::AcqRel) {
-    //         unsafe { (*self.value.get()).assume_init_drop() }
-    //         Ok(())
-    //     } else {
-    //         Err(())
-    //     }
-    // }
-
     #[inline]
     pub fn empty(&self) {
-        // Wait for guard to drop
+        // Wait for the guard to get dropped
         let mut lock = unsafe { self.value.write().unwrap_unchecked() };
         // set tag to false, drop the inner if it was already written
         if self.tag.fetch_and(false, Ordering::AcqRel) {
             unsafe { lock.assume_init_drop() }
         }
     }
-
-    // #[inline]
-    // fn wait_acq_guard(&self) {
-    //     // while self
-    //     // .guard
-    //     // .compare_exchange(false, true, Ordering::AcqRel, Ordering::Relaxed)
-    //     // .is_err()
-    //     while self.guard.fetch_or(true, Ordering::AcqRel) {
-    //         // println!("spinning at {}", line!());
-    //         spin_loop();
-    //     }
-    // }
 }
 
 impl<'a, T> SetGuard<'a, T> {
