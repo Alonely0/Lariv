637ef46951cb6bb424886345f56f2d89b320bc4a
guillem <4lon3ly0@tutanota.com>
2023-04-14 18:20:38 +0200

    Add & improve tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,19 +1,39 @@
-use std::thread::scope;
+use std::{
+    sync::{
+        Mutex,
+    },
+    thread::scope,
+};
 
 use crate::{Lariv, LarivIndex};
 
 #[test]
-pub fn all_of_it() {
-    let buffer = Lariv::new(10);
-    let buf = &buffer;
+pub fn general() {
+    let lariv = Lariv::new(10);
+    let buf = &lariv;
     scope(|s| {
         for i in 1..=100 {
-            let li = LarivIndex::new((i as f64 / 10.0).abs() as usize, i % 10);
-            println!("{li:?}");
+            let li = LarivIndex::new(i / 10, i - 1);
             s.spawn(move || buf.push(i));
             s.spawn(move || buf.get(li));
             s.spawn(move || buf.remove(li));
         }
     });
-    println!("{buffer:?}");
+}
+
+#[test]
+pub fn correctness() {
+    let lariv = Lariv::new(10);
+    let buf = &lariv;
+    let mut r = Mutex::new(Vec::<(LarivIndex, String)>::with_capacity(100));
+    let c = &r;
+    scope(|s| {
+        for i in 1..=100 {
+            s.spawn(move || c.lock().unwrap().push((buf.push(i.to_string()), i.to_string())));
+        }
+    });
+    for (i, e) in r.get_mut().unwrap().iter() {
+        assert_eq!(*e, *lariv.get(*i).unwrap().read().unwrap());
+        lariv.remove(*i)
+    }
 }
