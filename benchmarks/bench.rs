a920054a55fa8e776d085194aaad4d66725d51f4
guillem <4lon3ly0@tutanota.com>
2023-08-03 21:48:00 +0200

    Update benchmark algorithm This one is more realistic when it comes to Lariv's use case, and helps prove the advantages of better cache locality (which is a silly thing to say because Lariv is a glorified linked list).

--- benchmarks/bench.rs
+++ benchmarks/bench.rs
@@ -12,12 +12,13 @@ criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
 
 fn bench() {
-    let buffer = black_box(ManuallyDrop::new(Lariv::new(black_box(8000))));
+    let data = black_box(BigArray([0; 4096]));
+    let buffer = black_box(ManuallyDrop::new(Lariv::<BigArray>::new(5000)));
     let buf = &buffer;
     scope(|s| {
         for i in 1..=10000 {
             let li = LarivIndex::new(i / 5000, (i % 5000) - 1);
-            s.spawn(move || buf.push(i));
+            s.spawn(move || buf.push(data));
             s.spawn(move || {
                 buf.get(li);
             });
@@ -27,13 +28,20 @@ fn bench() {
 }
 
 fn bench2() {
+    let data = black_box(BigArray([0; 4096]));
     let buffer = black_box(ManuallyDrop::new(DashMap::new()));
     let buf = &buffer;
     scope(|s| {
         for i in 1..10000 {
-            s.spawn(move || buf.insert(i, i));
-            s.spawn(move || buf.get(&(i - 1)).map(black_box));
+            s.spawn(move || buf.insert(i, data));
+            s.spawn(move || {
+                buf.get(&(i - 1));
+            });
             s.spawn(move || buf.remove(&(i - 1)));
         }
     });
 }
+
+#[repr(C, align(16))]
+#[derive(Clone, Copy)]
+struct BigArray([u8; 4096]);
